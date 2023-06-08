05ec741cedd849522da165a57acb04e2a9930cd7
Yang Liu <liu00605@algongquinlive.com>
2023-06-07 22:52:11 -0400

    Commit: Added camera permission.

--- app/src/main/java/com/example/myapplication1/SecondActivity.java
+++ app/src/main/java/com/example/myapplication1/SecondActivity.java
@@ -7,13 +7,22 @@ import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+
+
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -119,13 +128,24 @@ public class SecondActivity extends AppCompatActivity {
 
         binding.button2.setOnClickListener((v) -> {
             //Log.e(TAG, "clicked CHANGE PICTURE.");
+
             Intent cameraIntent  = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
 
-            mCameraResult.launch(cameraIntent);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+                     mCameraResult.launch(cameraIntent);
+                    //startActivity(cameraIntent);
+                else
+                    requestPermissions(new String[] {Manifest.permission.CAMERA}, 20);
+            }
 
-            //startActivity(cameraIntent);
+
+
+
+           // startActivity(cameraIntent);
         });
+
 
 
 //        binding.button2.setOnClickListener(cl-> {
@@ -155,4 +175,18 @@ public class SecondActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if ((requestCode == 20) && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                mCameraResult.launch(cameraIntent);
+            startActivity( new Intent(MediaStore.ACTION_IMAGE_CAPTURE) );
+        }
+    }
+
+
 }
